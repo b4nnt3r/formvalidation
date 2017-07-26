17cac81e17d2e078209a25bc8eee207f748dc570
Brandon Walden <brandonshanewalden@gmail.com>
2017-07-26 11:25:28 -0400

    added form and data collection

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
 
 var app = express();
 
+//Configure mustache with Express
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
+//Allows public folder to be served statically to browsers
+app.use(express.static('public'));
+
+//Configure body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', function(request, response) {
-  response.render('index');
+  response.render('form');
+});
+
+app.post('/', function(request, response) {
+  response.render('answers', { answers: request.body });
 });
 
 app.listen(3000, function(){
